b65563be0a3f6ba541b9c257becbbc7084ae74f6
GovindPatil75 <mulegr75@gmail.com>
2024-05-09 09:17:24 +0530

    Session 3

--- src/test/java/com/Test/FirstAutomationTest.java
+++ src/test/java/com/Test/FirstAutomationTest.java
@@ -19,11 +19,28 @@ public class FirstAutomationTest {
 	// get()
 		driver.get("https://www.facebook.com/");
 		
+		//maximize
+				driver.manage().window().maximize();
+				
+				// title webpage
+				
+				String Title=driver.getTitle();
+				System.out.println(Title);
+				
+				String Url=driver.getCurrentUrl();
+				System.out.println(Url);
+				
+				// browser current window -close
+				
+				//driver.close();
+				
+				// current window + all open window -- close 
+				driver.quit();
 		
 		//Edge Browser
-		WebDriverManager.edgedriver().setup();
-		EdgeDriver driver1=new EdgeDriver();
-		driver1.get("https://www.facebook.com/");
+		//WebDriverManager.edgedriver().setup();
+		//EdgeDriver driver1=new EdgeDriver();
+		//driver1.get("https://www.facebook.com/");
 		
 		
 		
