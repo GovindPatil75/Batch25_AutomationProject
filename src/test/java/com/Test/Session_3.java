402abc87f76783cfd5a253c209e806ef19a069c1
GovindPatil75 <mulegr75@gmail.com>
2024-05-16 09:09:09 +0530

    Session 4

---
+++ src/test/java/com/Test/Session_3.java
@@ -0,0 +1,37 @@
+package com.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Session_3 {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+		// identify WebElement -- Absolute Xpath (/) --parent node navigate immidate child 
+		//Adv -- dyamic WebElement identify 
+		
+		//DisAdv -- 1. absolute expression lengthly 
+		           // 2. UI changes -- xpath collapse 
+		
+		WebElement Email=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input"));
+		Email.sendKeys("Test@gmail.com");
+		
+		
+		WebElement password=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input"));
+		password.sendKeys("12345678");
+		
+		WebElement loginBtn=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button"));
+		loginBtn.click();
+		
+		
+	}
+
+}
