76f03e4d927f067ecf2a42dfc2199b66e2691bea
GovindPatil75 <mulegr75@gmail.com>
2024-06-19 09:25:06 +0530

    Cross Browser Test Session

---
+++ src/test/java/com/CorssBrowserTest/VerfiyHomePageTest.java
@@ -0,0 +1,12 @@
+package com.CorssBrowserTest;
+
+import org.testng.annotations.Test;
+
+public class VerfiyHomePageTest extends BaseClass {
+	
+	@Test
+	public void TC_002() {
+		System.out.println("Test 002");
+	}
+
+}
---
+++ src/test/java/com/CorssBrowserTest/VerifyTestLogin.java
@@ -0,0 +1,12 @@
+package com.CorssBrowserTest;
+
+import org.testng.annotations.Test;
+
+public class VerifyTestLogin extends BaseClass{
+
+	
+	@Test
+	public void TC_001() {
+		System.out.println("Test 001");
+	}
+}
